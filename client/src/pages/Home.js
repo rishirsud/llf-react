c67912208b1bb324441d2b921efe18427c54a084
rishi <rishirsud@gmail.com>
2019-06-04 10:44:18 -0400

    removed unused components from home and profile page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import Hero from '../Components/Hero';
-import Navbar from '../Components/Navbar';
 import Search from '../Components/Search';
 
 
 
 class homePage extends Component {
 
-
-
-
-
-
-
-
   render() {
     return (
       <React.Fragment>
-        {/* <Navbar /> */}
         <Hero>
           <Search />
         </Hero>
       </React.Fragment>
     )
   }
-
-
 
 }
 export default homePage;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,27 +3,6 @@ import Hero from '../Components/Hero';
 import ProfileCard from '../Components/ProfileCard'
 
 
-// class profilePage extends Component {
-
-
-
-
-
-
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Hero />
-//         <ProfileCard />
-
-
-
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 function profilePage() {
   return (
     <React.Fragment>
