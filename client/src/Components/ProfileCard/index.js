359c484f106dff6ac498a07be9cc472295cdb24e
rishi <rishirsud@gmail.com>
2019-06-04 13:41:41 -0400

    cleared errors from console

--- client/src/Components/ProfileCard/index.js
+++ client/src/Components/ProfileCard/index.js
@@ -4,59 +4,59 @@ import './style.css';
 function ProfileCard() {
   return (
   <React.Fragment>
-  <div class="container" id="profileContainer">
-    <div class="row">
-      <div class="col-lg-10 col-12 my-3 mx-auto">
+  <div className="container" id="profileContainer">
+    <div className="row">
+      <div className="col-lg-10 col-12 my-3 mx-auto">
         
         <form action="">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Your Profile</h5>
-              <p class="card-text">Only information you input here will be visible. All fields are optional. If you want
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Your Profile</h5>
+              <p className="card-text">Only information you input here will be visible. All fields are optional. If you want
                 to find your friends, it's suggested you fill some of them out.</p>
 
-              <div class="row">
-                <div class="col-lg-4 col-12 mx-auto">
-                  <div class="form-group">
+              <div className="row">
+                <div className="col-lg-4 col-12 mx-auto">
+                  <div className="form-group">
                     <label for="email">Your Email </label>
-                    <input type="email" class="form-control" id="profileEmail" placeholder="Your Email" disabled />
+                    <input type="email" className="form-control" id="profileEmail" placeholder="Your Email" disabled />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <label for="firstName">First Name* </label>
-                    <input type="text" class="form-control" id="firstName" placeholder="Enter first name" />
+                    <input type="text" className="form-control" id="firstName" placeholder="Enter first name" />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <label for="location">Enter your general location* </label>
-                    <input type="text" class="form-control" id="location" placeholder="Country/State" />
+                    <input type="text" className="form-control" id="location" placeholder="Country/State" />
                   </div>
                 </div>
 
 
-                <div class="col-lg-4 col-12 mx-auto">
-                  <div class="form-group">
+                <div className="col-lg-4 col-12 mx-auto">
+                  <div className="form-group">
                     <label for="steamID">Steam ID* </label>
-                    <input type="text" class="form-control" id="steamID" placeholder="Enter Steam ID: " />
+                    <input type="text" className="form-control" id="steamID" placeholder="Enter Steam ID: " />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <label for="psnID">PSN ID* </label>
-                    <input type="text" class="form-control" id="psnID" placeholder="Enter PSN ID" />
+                    <input type="text" className="form-control" id="psnID" placeholder="Enter PSN ID" />
                   </div>
-                  <div class="form-group">
+                  <div className="form-group">
                     <label for="xboxID">Xbox ID* </label>
-                    <input type="text" class="form-control" id="xboxID" placeholder="Enter Xbox ID" />
+                    <input type="text" className="form-control" id="xboxID" placeholder="Enter Xbox ID" />
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col text-right">
-                  <small id="optionalInfo" class="form-text text-muted">* field is optional</small>
+              <div className="row">
+                <div className="col text-right">
+                  <small id="optionalInfo" className="form-text text-muted">* field is optional</small>
                 </div>
               </div>
             </div>
 
-            <div class="row">
-              <div class="col-12 my-2 text-center mx-auto">
-                <button id="saveProfile" class="btn btn-primary">Save</button>
+            <div className="row">
+              <div className="col-12 my-2 text-center mx-auto">
+                <button id="saveProfile" className="btn btn-primary">Save</button>
               </div>
             </div>
           </div>
