a170fa961628d50099ad831124c722344b080ea2
Steve A <steve.adamcsik@gmail.com>
2019-06-05 10:15:26 -0400

    saving

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-
+import './style.css';
 
 
 function Navbar() {
