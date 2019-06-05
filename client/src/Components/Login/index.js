4f1d57bd11d21ead948cf69ee3d1d12559e6785f
rishi <rishirsud@gmail.com>
2019-06-05 13:42:38 -0400

    login/logout functions and states are global

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import './style.css';
 
-class Login extends React.Component {
+class Login extends Component {
 
   state = {
     email: '',
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Hero from '../Components/Hero';
 import ProfileCard from '../Components/ProfileCard'
+import { Redirect } from 'react-router'
+// import registerPage from './pages/Register';
 
 
 function profilePage(props) {
@@ -13,8 +15,8 @@ function profilePage(props) {
         <ProfileCard />
       </React.Fragment>
     ) : (
-        <div>Hi!</div>
         // REDIRECT
+        <Redirect to="/register" />
       )
   )
 }
